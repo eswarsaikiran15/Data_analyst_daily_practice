585e9aba22a024ef0c9644b0e60e867fa6c6173b
K.ESWAR SAI KIRAN <160008710+eswarsaikiran15@users.noreply.github.com>
2026-05-02 16:37:56 +0530

    Create order_by.sql

---
+++ My_Sql/order_by.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE KIRAN;
+USE KIRAN;
+
+-- Create table
+CREATE TABLE student (
+    rollno INT PRIMARY KEY,
+    name VARCHAR(50),
+    marks INT NOT NULL,
+    grade VARCHAR(1),
+    city VARCHAR(20)
+);
+
+-- Insert multiple records
+INSERT INTO student (rollno, name, marks, grade, city)
+VALUES
+(101, 'anil', 78, 'C', 'Pune'),
+(102, 'bhumika', 93, 'A', 'Mumbai'),
+(103, 'chetan', 85, 'B', 'Mumbai'),
+(104, 'dhruv', 96, 'A', 'Delhi'),
+(105, 'emanuel', 12, 'F', 'Delhi'),
+(106, 'farah', 82, 'B', 'Delhi');
+
+-- View data
+SELECT * FROM student;
+
+
+-- limit clause( set an  upper limit  on number of (tuple) rows to be returned
+select * from student limit 3; 
+
+-- order by clause to sort in asc or desc order
+select * from student 
+order by city asc;
+-- order by clause to sort in asc or desc order
+select * from student 
+order by city desc;
+
+-- order by clause to sort in asc or desc order
+select * from student 
+order by marks asc
+;
+
+select * from student 
+order by marks desc
+limit 3 
+;
+
